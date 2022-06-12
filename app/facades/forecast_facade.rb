2617de24f2d1bf42e38622d82742206e229c2e5b
James Harkins <jh.harkins@protonmail.com>
2022-06-11 20:26:51 -0600

    Feat: pass tests for ForecastFacade#generate_forecast class method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,4 +1,6 @@
 class ForecastFacade
   def self.generate_forecast(coordinates)
+    data = OpenweatherService.get_forecast_data(coordinates)
+    Forecast.new(data)
   end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe ForecastFacade do
   describe "class methods" do
     describe "#generate_forecast" do
-      it "returns a Forecast PORO for some set of coordinates" do
+      it "returns a Forecast PORO for some set of coordinates", :vcr do
         denver_coordinates = {lat: 39.738453, lon: -104.984853}
         denver_forecast = ForecastFacade.generate_forecast(denver_coordinates)
 
