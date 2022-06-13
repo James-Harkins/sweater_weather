2b5161c3291b7f34b47555320e1b7ee127c40f2d
James Harkins <jh.harkins@protonmail.com>
2022-06-13 13:26:54 -0600

    Test: create sad path test for POST /api/v1/sessions to return a 400 error with a generic error message if the user is not authenticated

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -27,5 +27,23 @@ describe "sessions request" do
       expect(session_user[:attributes]).to have_key(:api_key)
       expect(session_user[:attributes][:api_key]).to be_a String
     end
+
+    it "returns a 400 error if that user cannot be authenticated" do
+      user = User.create!(email: "test@testing.com", password: "test123", password_confirmation: "test123")
+
+      json_payload = {
+        email: "test@testing.com",
+        password: "bad_password"
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/sessions", headers: headers, params: json_payload.to_json, as: :json
+
+      expect(response).to have_http_status(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:error]).to eq("Invalid credentials.")
+    end
   end
 end
