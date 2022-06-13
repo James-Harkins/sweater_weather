07a938053873b41f1da50a96fc6da51fa35965d7
James Harkins <jh.harkins@protonmail.com>
2022-06-13 10:10:46 -0600

    Feat: pass test for BooksFacade#get_total_search_results_by_location class method

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -3,4 +3,8 @@ class BooksFacade
     books_data = OpenLibraryService.get_books_by_location("denver,co")[:docs][0..(quantity - 1)]
     books_data.map { |book_data| Book.new(book_data) }
   end
+
+  def self.get_total_search_results_by_location(location)
+    OpenLibraryService.get_books_by_location(location)[:numFound]
+  end
 end
