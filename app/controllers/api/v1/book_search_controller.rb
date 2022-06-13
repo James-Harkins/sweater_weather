2df15b06b8e4d0a68aaa94f51a9f308f80bdf515
James Harkins <jh.harkins@protonmail.com>
2022-06-13 11:01:04 -0600

    Feat: pass sad path test for a quantity param less than zero

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::BookSearchController < ApplicationController
   def index
     if params[:quantity].to_i > 0
       render json: BooksAndForecastSerializer.serialize_books_and_forecast_by_location(params[:location], params[:quantity])
+    else
+      render json: {error: "Quantity must be greater than zero."}, status: 400
     end
   end
 end
