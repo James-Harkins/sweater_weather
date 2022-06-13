cf4fb48017f01268a5a27c201aa5a98fbef87dba
James Harkins <jh.harkins@protonmail.com>
2022-06-13 15:17:24 -0600

    Test: create tests for TravelGuideFacade#generate_travel_guide class method

---
+++ spec/facades/travel_guide_facade_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe TravelGuideFacade do
+  describe "class methods" do
+    describe "#generate_travel_guide" do
+      it "returns a TravelGuide PORO based on an origin and destination passed in as arguments", :vcr do
+        travel_guide = TravelGuideFacade.generate_travel_guide("Denver, CO", "Pueblo, CO")
+        pueblo_coordinates = {
+          lon: -104.610413,
+          lat: 38.265427
+        }
+
+        expect(travel_guide).to be_a TravelGuide
+        expect(travel_guide.travel_time).to eq(7487)
+        expect(travel_guide.destination_coordinates).to eq(pueblo_coordinates)
+      end
+    end
+  end
+end
