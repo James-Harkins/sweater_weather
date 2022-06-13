a6e341d20d198a582d27285405ad3321dc968e49
James Harkins <jh.harkins@protonmail.com>
2022-06-13 09:52:32 -0600

    Feat: pass tests for Book PORO

---
+++ app/poros/book.rb
@@ -0,0 +1,8 @@
+class Book
+  attr_reader :title, :isbn, :publisher
+  def initialize(attributes)
+    @title = attributes[:title]
+    @isbn = attributes[:isbn]
+    @publisher = attributes[:publisher]
+  end
+end
