72d481241ed56254ea4b649a705651853207e6fe
James Harkins <jh.harkins@protonmail.com>
2022-06-13 09:57:20 -0600

    Test: create tests for BooksFacde#get_quantity_of_books_by_location class method

---
+++ spec/facades/books_facade_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe BooksFacade do
+  describe "class methods" do
+    describe "#get_quantity_of_books_by_location" do
+      it "returns a quantity of Book POROs by some location search param" do
+        five_denver_books = BooksFacade.get_quantity_of_books_by_location("denver,co", 5)
+
+        expect(five_denver_books).to be_all Book
+        expect(five_denver_books.count).to eq(5)
+        expect(five_denver_books[0].title).to eq("Denver, Co")
+        expect(five_denver_books[0].isbn).to eq(["9780762507849", "0762507845"])
+        expect(five_denver_books[0].publisher).to eq(["Universal Map Enterprises"])
+      end
+    end
+  end
+end
