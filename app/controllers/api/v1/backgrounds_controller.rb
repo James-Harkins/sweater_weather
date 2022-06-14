be867744080ff43d0279e06898cec3542e31dd27
James Harkins <jh.harkins@protonmail.com>
2022-06-14 14:34:51 -0600

    Refactor: move road_trip_data hash logic from RoadTripController to TravelGuideFacade for better abstraction

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
     image = BackgroundImageFacade.generate_background_image(params[:location])
-    render json: BackgroundImageSerializer.serialize_image(image, params[:location])
+    render json: BackgroundImageSerializer.serialize(image, params[:location])
   end
 end
--- app/facades/travel_guide_facade.rb
+++ app/facades/travel_guide_facade.rb
@@ -3,4 +3,15 @@ class TravelGuideFacade
     directions_data = MapquestService.get_directions(origin, destination)
     TravelGuide.new(directions_data)
   end
+
+  def self.generate_road_trip_data(origin, destination)
+    travel_guide = generate_travel_guide(origin, destination)
+    destination_forecast = ForecastFacade.generate_forecast_by_travel_guide(travel_guide)
+    {
+      origin: origin,
+      destination: destination,
+      travel_guide: travel_guide,
+      forecast: destination_forecast
+    }
+  end
 end
