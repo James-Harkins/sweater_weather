21e46cd97a97a031bdaea43907b77345c79e2858
James Harkins <jh.harkins@protonmail.com>
2022-06-14 14:04:48 -0600

    Refactor: move ForecastFacade.generate_forecast_by_travel_guide from RoadTripSerializer to RoadTripController to be passed to Serializer as argument

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,10 +3,12 @@ class Api::V1::RoadTripController < ApplicationController
     request_params = JSON.parse(params["_json"], symbolize_names: true)
     if User.find_by(api_key: request_params[:api_key])
       travel_guide = TravelGuideFacade.generate_travel_guide(request_params[:origin], request_params[:destination])
+      destination_forecast = ForecastFacade.generate_forecast_by_travel_guide(travel_guide)
       road_trip_data = {
         origin: request_params[:origin],
         destination: request_params[:destination],
-        travel_guide: travel_guide
+        travel_guide: travel_guide,
+        forecast: destination_forecast
       }
       render json: RoadTripSerializer.serialize(road_trip_data), status: :created
     else
--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,10 +1,5 @@
 class RoadTripSerializer
   def self.serialize(road_trip_data)
-    # travel_guide = TravelGuideFacade.generate_travel_guide(origin, destination)
-    # destination_coordinates = travel_guide.destination_coordinates
-    # if destination_coordinates
-    #   destination_forecast = ForecastFacade.generate_forecast(destination_coordinates)
-    # end
     {
       data: {
         id: nil,
@@ -13,7 +8,7 @@ class RoadTripSerializer
           start_city: road_trip_data[:origin],
           end_city: road_trip_data[:destination],
           travel_time: road_trip_data[:travel_guide].format_travel_time,
-          weather_at_eta: ForecastFacade.generate_forecast_by_travel_guide(road_trip_data[:travel_guide])
+          weather_at_eta: road_trip_data[:forecast]
         }
       }
     }
