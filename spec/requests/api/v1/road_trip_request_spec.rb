593285db66dcc2dcdfae1bda41009043fef614cd
James Harkins <jh.harkins@protonmail.com>
2022-06-14 09:21:23 -0600

    Test: create tests for international road trip requests

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -59,5 +59,27 @@ describe "road trip request" do
       expect(road_trip[:attributes][:weather_at_eta][:temperature]).to be_a String
       expect(road_trip[:attributes][:weather_at_eta][:conditions]).to be_a String
     end
+
+    it "will show an impossible travel time and an empty weather block for international travel", :vcr do
+      user = User.create!(email: "test@testing.com", password: "test123", password_confirmation: "test123")
+
+      json_payload = {
+        origin: "Denver, CO",
+        destination: "London, UK",
+        api_key: user.api_key
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/road_trip", headers: headers, params: json_payload.to_json, as: :json
+
+      expect(response).to have_http_status(201)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+      road_trip = response_body[:data]
+
+      expect(road_trip[:attributes][:travel_time]).to eq("Impossible")
+      expect(road_trip[:attributes][:weather_at_eta][:temperature]).to eq("")
+      expect(road_trip[:attributes][:weather_at_eta][:conditions]).to eq("")
+    end
   end
 end
